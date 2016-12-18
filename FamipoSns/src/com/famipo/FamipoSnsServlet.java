b596e568b664d342f06ab89e8e7d05ef1efd95e2
Owner <Owner@192.168.1.7>
2016-12-18 21:10:47 +0900

    修正

--- FamipoSns/src/com/famipo/FamipoSnsServlet.java
+++ FamipoSns/src/com/famipo/FamipoSnsServlet.java
@@ -1,12 +1,16 @@
 package com.famipo;
 
 import java.io.IOException;
-import javax.servlet.http.*;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @SuppressWarnings("serial")
 public class FamipoSnsServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		resp.setContentType("text/plain");
 		resp.getWriter().println("Hello, world");
+		resp.getWriter().println("famipo");
 	}
 }
